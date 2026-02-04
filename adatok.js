32223d25588d1ae361d65af651e23421d15b0ce9
oroszbalint1910 <oroszbalint1910@gmail.com>
2026-02-04 19:37:39 +0100

    Add files via upload

--- adatok.js
+++ adatok.js
@@ -12,5 +12,12 @@ const ellatasok = [
       "/me leöblíti a sebet.",
       "/do A bőr kipirult, de nem mély."
     ]
+    {
+    cim: "Testierőszak",
+    szoveg: [
+      "/me előveszi a fertőtlenítőt.",
+      "/do A seb tiszta, nem fertőzött."
+    ]
+  },
   }
 ];
